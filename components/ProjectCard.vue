c0fca0411019aa577c28b68b877a5042938c855e
Jai A <jai.a@tuta.io>
2020-11-30 10:22:58 -0700

    Finish mod pages

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -81,7 +81,7 @@
               </p>
             </div>
           </div>
-          <div class="stat">
+          <div v-if="latestVersion" class="stat">
             <TagIcon />
             <div class="info">
               <h4>Available For</h4>
@@ -91,64 +91,7 @@
             </div>
           </div>
         </div>
-        <div class="categories">
-          <p v-if="categories.includes('fabric')">
-            <FabricLoader />
-            Fabric
-          </p>
-          <p v-if="categories.includes('forge')">
-            <ForgeLoader />
-            Forge
-          </p>
-          <p v-if="categories.includes('technology')">
-            <TechCategory />
-            Technology
-          </p>
-          <p v-if="categories.includes('adventure')">
-            <AdventureCategory />
-            Adventure
-          </p>
-          <p v-if="categories.includes('magic')">
-            <MagicCategory />
-            Magic
-          </p>
-          <p v-if="categories.includes('utility')">
-            <UtilityCategory />
-            Utility
-          </p>
-          <p v-if="categories.includes('decoration')">
-            <DecorationCategory />
-            Decoration
-          </p>
-          <p v-if="categories.includes('library')">
-            <LibraryCategory />
-            Library
-          </p>
-          <p v-if="categories.includes('cursed')">
-            <CursedCategory />
-            Cursed
-          </p>
-          <p v-if="categories.includes('worldgen')">
-            <WorldGenCategory />
-            Worldgen
-          </p>
-          <p v-if="categories.includes('storage')">
-            <StorageCategory />
-            Storage
-          </p>
-          <p v-if="categories.includes('food')">
-            <FoodCategory />
-            Food
-          </p>
-          <p v-if="categories.includes('equipment')">
-            <EquipmentCategory />
-            Equipment
-          </p>
-          <p v-if="categories.includes('misc')">
-            <MiscCategory />
-            Misc
-          </p>
-        </div>
+        <Categories :categories="categories" />
       </div>
     </div>
     <div v-if="editMode" class="buttons">
@@ -160,44 +103,17 @@
 </template>
 
 <script>
+import Categories from '@/components/Categories'
+
 import CalendarIcon from '~/assets/images/utils/calendar.svg?inline'
 import DownloadIcon from '~/assets/images/utils/download.svg?inline'
 import EditIcon from '~/assets/images/utils/edit.svg?inline'
 import TagIcon from '~/assets/images/utils/tag.svg?inline'
 
-import TechCategory from '~/assets/images/categories/tech.svg?inline'
-import AdventureCategory from '~/assets/images/categories/adventure.svg?inline'
-import CursedCategory from '~/assets/images/categories/cursed.svg?inline'
-import DecorationCategory from '~/assets/images/categories/decoration.svg?inline'
-import EquipmentCategory from '~/assets/images/categories/equipment.svg?inline'
-import FoodCategory from '~/assets/images/categories/food.svg?inline'
-import LibraryCategory from '~/assets/images/categories/library.svg?inline'
-import MagicCategory from '~/assets/images/categories/magic.svg?inline'
-import MiscCategory from '~/assets/images/categories/misc.svg?inline'
-import StorageCategory from '~/assets/images/categories/storage.svg?inline'
-import UtilityCategory from '~/assets/images/categories/utility.svg?inline'
-import WorldGenCategory from '~/assets/images/categories/worldgen.svg?inline'
-
-import ForgeLoader from '~/assets/images/categories/forge.svg?inline'
-import FabricLoader from '~/assets/images/categories/fabric.svg?inline'
-
 export default {
   name: 'ProjectCard',
   components: {
-    TechCategory,
-    AdventureCategory,
-    CursedCategory,
-    DecorationCategory,
-    EquipmentCategory,
-    FoodCategory,
-    LibraryCategory,
-    MagicCategory,
-    MiscCategory,
-    StorageCategory,
-    UtilityCategory,
-    WorldGenCategory,
-    ForgeLoader,
-    FabricLoader,
+    Categories,
     CalendarIcon,
     DownloadIcon,
     EditIcon,
@@ -246,7 +162,7 @@ export default {
     },
     latestVersion: {
       type: String,
-      default: 'None',
+      default: null,
     },
     categories: {
       type: Array,
@@ -307,16 +223,12 @@ export default {
         font-size: var(--font-size-lg);
       }
       .author {
-        margin: 0;
-        margin-left: 0.5rem;
-        margin-top: auto;
+        margin: auto 0 0 0.5rem;
         color: var(--color-text);
       }
     }
     .description {
-      margin: 0;
-      margin-top: var(--spacing-card-sm);
-      margin-right: var(--spacing-card-md);
+      margin: var(--spacing-card-sm) var(--spacing-card-md) 0 0;
       height: 100%;
       color: var(--color-text-dark);
     }
@@ -343,61 +255,15 @@ export default {
         @media screen and (min-width: 900px) {
           flex-wrap: nowrap;
         }
-        .stat {
-          margin-top: 0.5rem;
-          margin-right: 1rem;
-          @extend %row;
 
-          @media screen and (min-width: 900px) {
-            margin-top: 0;
-          }
-          svg {
-            margin: auto 0;
-            margin-right: 0.5rem;
-            height: 1.5rem;
-            width: 1.5rem;
-            color: var(--color-icon);
-          }
-          .info {
-            margin: auto 0;
-            white-space: nowrap;
-            h4 {
-              @extend %small-label;
-            }
-            .value {
-              font-size: var(--font-size-sm);
-              margin: 0;
-              color: var(--color-text-dark);
-            }
-          }
+        .stat {
+          @extend %stat;
         }
       }
       .categories {
-        @extend %row;
-        flex-wrap: wrap;
-        margin-top: var(--spacing-card-sm);
-
         @media screen and (min-width: 1024px) {
           flex-direction: row;
           margin: auto 0;
-        }
-        p {
-          display: flex;
-          align-items: center;
-          flex-direction: row;
-          background-color: var(--color-category-bg);
-          border-radius: var(--size-rounded-max);
-          color: var(--color-category-text);
-          margin-top: 0.25em;
-          margin-bottom: 0.25em;
-          margin-right: 0.5em;
-          padding: 0.4em 0.7em;
-          font-size: var(--font-size-sm);
-          height: 1em;
-          svg {
-            width: 15px;
-            margin-right: 5px;
-          }
         }
       }
     }
