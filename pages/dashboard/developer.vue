743a8fb0c4f7862340ce08ab1a58a727df87ae91
CyaCal <calumproh28@gmail.com>
2020-12-21 20:09:24 +0000

    Remove PopUp and use inline version

--- pages/dashboard/developer.vue
+++ pages/dashboard/developer.vue
@@ -3,54 +3,41 @@
     <div class="section-header columns">
       <h3 class="column-grow-1">Developer Tools</h3>
     </div>
-    <section class="essentials">
-      <h3>Authorization Token</h3>
-      <label>
-        <span>
-          Your authorization token can be used with the Modrinth API and for the
-          Minotaur Gradle plugin. However, it must be kept secret!
-        </span>
-        <button @click="copyToken">Copy to clipboard.</button>
-      </label>
-      <h3>Revoke your token</h3>
-      <label>
-        <span
-          >Beware, this will log you out of Modrinth, and you will have to login
-          again to access modrinth with a new token.</span
-        >
-        <button @click="showDelPopup = !showDelPopup">Revoke Token</button>
-      </label>
-    </section>
-    <Popup :show-popup="showDelPopup" class="del-token-popup-body">
-      <h3>How to revoke your modrinth token.</h3>
-      <p>
-        First of all,
-        <a
-          class="revk-link"
-          target="_blank"
-          href="https://github.com/settings/connections/applications/3acffb2e808d16d4b226"
-          >head to the Modrinth Application page on Github.</a
-        ><br />
-        Then press the <strong>Revoke access</strong> button found next to the
-        <strong>Permissions</strong> header.<br /><br />
-        <a class="revk-link" href="https://iili.io/Kcq4wu.png" target="_blank"
-          >See this screenshot if confused.</a
-        >
-      </p>
-      <p>Once you have completed this, press the continue button below.</p>
-      <button @click="showDelPopup = !showDelPopup">Cancel</button>
-      <button @click="logout">Continue</button>
-    </Popup>
+    <modal v-show="main_component">
+      <section class="essentials">
+        <h3>Authorization Token</h3>
+        <label>
+          <span>
+            Your authorization token can be used with the Modrinth API and for
+            the Minotaur Gradle plugin. However, it must be kept secret!
+          </span>
+          <button @click="copyToken">Copy to clipboard.</button>
+        </label>
+        <h3>Revoke your token</h3>
+        <label>
+          <span
+            >Beware, this will log you out of Modrinth, and you will have to
+            login again to access modrinth with a new token.</span
+          >
+          <button @click="showDelPopup">Revoke Token</button>
+        </label>
+      </section>
+    </modal>
+    <modal v-show="revoke_component">
+      <RevokeTokenHint @wantClose="showDelPopup"></RevokeTokenHint>
+    </modal>
   </DashboardPage>
 </template>
 
 <script>
 import DashboardPage from '@/components/DashboardPage'
+import RevokeTokenHint from '~/components/RevokeTokenHint.vue'
 // import axios from 'axios'
 
 export default {
   components: {
     DashboardPage,
+    RevokeTokenHint,
   },
   fetch() {
     this.token = this.$auth.getToken('local')
@@ -58,7 +45,8 @@ export default {
   data() {
     return {
       token: '',
-      showDelPopup: false,
+      revoke_component: false,
+      main_component: true,
     }
   },
   methods: {
@@ -91,15 +79,22 @@ export default {
         type: 'success',
       })
     },
-    async logout() {
-      await this.$auth.setToken('local', false)
-      await this.$router.go('/')
+    showDelPopup() {
+      this.revoke_component = !this.revoke_component
+      this.main_component = !this.main_component
     },
   },
 }
 </script>
 
 <style lang="scss" scoped>
+.del-token-popup-body {
+  padding-top: var(--spacing-card-lg);
+  padding-bottom: var(--spacing-card-lg);
+  padding-left: var(--spacing-card-lg);
+  padding-right: var(--spacing-card-lg);
+}
+
 section {
   @extend %card;
   padding: var(--spacing-card-md) var(--spacing-card-lg);
