b8f736934877b0d9d654257f981dab8c37aa1c71
joaoh1 <joaoh1@users.noreply.github.com>
2021-01-19 17:49:56 -0300

    Fix summary tags being removed by XSS

--- plugins/compiled-markdown-directive.js
+++ plugins/compiled-markdown-directive.js
@@ -3,7 +3,9 @@ import xss from 'xss'
 import marked from 'marked'
 
 function compileMarkdown(target, markdown) {
-  target.innerHTML = xss(marked(markdown))
+  target.innerHTML = xss(marked(markdown), {
+    whiteList: Object.assign(xss.whiteList, { summary: [] }),
+  })
 }
 
 Vue.directive('compiled-markdown', {
