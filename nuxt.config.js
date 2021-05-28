ea1773d97bd8ed3427c3ef131e78ddab0f3e865a
Jai A <jaiagr+gpg@pm.me>
2021-05-28 13:52:24 -0700

    Move Host URL to top

--- nuxt.config.js
+++ nuxt.config.js
@@ -191,12 +191,13 @@ export default {
 
 function getDomain() {
   if (process.env.NODE_ENV === 'production') {
-    if (process.env.HEROKU_APP_NAME) {
+    if (process.env.HOST_URL) {
+      return process.env.HOST_URL
+    }
+    else if (process.env.HEROKU_APP_NAME) {
       return `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
     } else if (process.env.VERCEL_URL) {
       return `https://${process.env.VERCEL_URL}`
-    } else if (process.env.HOST_URL) {
-      return process.env.HOST_URL
     } else {
       return 'https://modrinth.com'
     }
