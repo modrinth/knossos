modrinth/code

Wrap mod icon and title in link (#273)
* Wrap mod icon and title in link

* Fixes #218

pages/mod/_id.vue
@@ -4,17 +4,20 @@
       <div class="content">
         <div class="header">
           <div class="icon">
-            <img
-              :src="
-                mod.icon_url
-                  ? mod.icon_url
-                  : 'https://cdn.modrinth.com/placeholder.svg?inline'
-              "
-              alt="mod - icon"
-            />
+            <nuxt-link :to="'/mod/' + (mod.slug ? mod.slug : mod.id)">
+              <img
+                :src="
+                  mod.icon_url
+                    ? mod.icon_url
+                    : 'https://cdn.modrinth.com/placeholder.svg?inline'
+                "
+                alt="mod - icon"
+            /></nuxt-link>
           </div>
           <div class="info">
-            <h1 class="title">{{ mod.title }}</h1>
+            <nuxt-link :to="'/mod/' + (mod.slug ? mod.slug : mod.id)">
+              <h1 class="title">{{ mod.title }}</h1>
+            </nuxt-link>
             <p class="description">
               {{ mod.description }}
             </p>

modrinth/knossos

pages/_type/_id.vue
@@ -3,16 +3,33 @@
     <div class="page-contents">
       <section class="project-info">
         <div class="header">
-          <img
-            class="icon"
-            :src="
-              project.icon_url
-                ? project.icon_url
-                : 'https://cdn.modrinth.com/placeholder.svg?inline'
+          <nuxt-link
+            :to="
+              '/' +
+              project.project_type +
+              '/' +
+              (project.slug ? project.slug : project.id)
             "
-            alt="project - icon"
-          />
-          <h1 class="title">{{ project.title }}</h1>
+          >
+            <img
+              class="icon"
+              :src="
+                project.icon_url
+                  ? project.icon_url
+                  : 'https://cdn.modrinth.com/placeholder.svg?inline'
+              "
+              alt="project - icon"
+          /></nuxt-link>
+          <nuxt-link
+            :to="
+              '/' +
+              project.project_type +
+              '/' +
+              (project.slug ? project.slug : project.id)
+            "
+          >
+            <h1 class="title">{{ project.title }}</h1>
+          </nuxt-link>
           <div
             v-if="
               project.client_side === 'optional' &&