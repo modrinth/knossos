modrinth/code

Fix a bug & add improvements to a couple moderation aspects (#278)
This PR fixes reports on the moderation dashboard going to `/dashboard/mod/_id` instead of to `/mod/_id`.
It also allows the ability for moderators to unlist mods in the queue from the frontend instead of having to do it via the backend.
![image](https://i.imgur.com/x8shSVn.png)
Unlisted mods should have the ability to resubmit for approval, so I've also changed "Submit for Review" to "Submit for approval", allowing unlisted mods to do that as well.
![image](https://i.imgur.com/OC8Vyfo.png)

pages/dashboard/moderation.vue
@@ -31,6 +31,12 @@
             Approve
           </button>
           <button
+            class="button column unlist"
+            @click="changeModStatus(mod.id, 'unlisted', index)"
+          >
+            Unlist
+          </button>
+          <button
             class="button column reject"
             @click="changeModStatus(mod.id, 'rejected', index)"
           >
@@ -53,7 +59,9 @@
           <h5 class="title">
             Report for {{ report.item_type }}
             <nuxt-link
-              :to="report.item_type + '/' + report.item_id.replace(/\W/g, '')"
+              :to="
+                '/' + report.item_type + '/' + report.item_id.replace(/\W/g, '')
+              "
               >{{ report.item_id }}
             </nuxt-link>
           </h5>

modrinth/knossos

pages/dashboard/moderation.vue
@@ -32,6 +32,12 @@
             Approve
           </button>
           <button
+            class="button column unlist"
+            @click="changeProjectStatus(project.id, 'unlisted', index)"
+          >
+            Unlist
+          </button>
+          <button
             class="button column reject"
             @click="changeProjectStatus(project.id, 'rejected', index)"
           >
@@ -54,7 +60,9 @@
           <h5 class="title">
             Report for {{ report.item_type }}
             <nuxt-link
-              :to="report.item_type + '/' + report.item_id.replace(/\W/g, '')"
+              :to="
+                '/' + report.item_type + '/' + report.item_id.replace(/\W/g, '')
+              "
               >{{ report.item_id }}
             </nuxt-link>
           </h5>