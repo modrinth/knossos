modrinth/code

Add indexing meta value depending on the status of the mod. (#261)

pages/mod/_id.vue
@@ -543,6 +543,11 @@ export default {
             ? this.mod.icon_url
             : 'https://cdn.modrinth.com/placeholder.png',
         },
+        {
+          hid: 'robots',
+          name: 'robots',
+          content: this.mod.status !== 'approved' ? 'noindex' : 'all',
+        },
       ],
     }
   },

modrinth/knossos

pages/_type/_id.vue
@@ -525,6 +525,11 @@ export default {
             ? this.project.icon_url
             : 'https://cdn.modrinth.com/placeholder.png',
         },
+        {
+          hid: 'robots',
+          name: 'robots',
+          content: this.project.status !== 'approved' ? 'noindex' : 'all',
+        },
       ],
     }
   },