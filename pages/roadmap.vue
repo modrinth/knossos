085202973cb1981bd715148db0cadf4029be485c
Mark Gross <mark.gross2001@gmail.com>
2021-11-14 10:23:47 -0800

    Add roadmap page

---
+++ pages/roadmap.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="page-container">
+    <div class="roadmap-content">
+      <h1>Roadmap</h1>
+      <p>
+        This is a listing of completed, planned, and in progress features for
+        Modrinth. An up-to-date roadmap can also be found in our
+        <a href="https://discord.gg/EUHuJHt">Discord server</a>!
+      </p>
+      <h2>Completed</h2>
+      <ul>
+        <li>Create an alternative mod hosting platform</li>
+        <li>Implement fast and accurate search</li>
+        <li>Be transparent and public</li>
+        <li>Create an open and robust API</li>
+        <li>Comply with the GDPR and protect your privacy</li>
+        <li>Provide quality-of-life features like notifications, and more</li>
+        <li>Allow creators to team up and manage permissions</li>
+      </ul>
+      <h2>In Progress</h2>
+      <ul>
+        <li>Implement modpacks and support for project types</li>
+        <li>A gallery system for projects to show off screenshots</li>
+        <li>An overhauled frontend</li>
+        <li>A way for projects to specify dependencies</li>
+        <li>Squash bugs and fix problems</li>
+        <li>Fully document our API</li>
+      </ul>
+      <h2>Planned</h2>
+      <ul>
+        <li>Pay creators a share of ads money</li>
+        <li>Automate platform migration process</li>
+        <li>Roll out our own authentication service</li>
+        <li>Provide analytics and stats to creators</li>
+        <li>And even more cool features that we haven't thought of yet!</li>
+      </ul>
+    </div>
+    <m-footer class="footer" centered padded />
+  </div>
+</template>
+<script>
+import MFooter from '~/components/layout/MFooter'
+export default {
+  components: {
+    MFooter,
+  },
+}
+</script>
+<style lang="scss" scoped>
+.roadmap-content {
+  margin-left: 15%;
+}
+
+a {
+  color: var(--color-brand);
+}
+</style>
