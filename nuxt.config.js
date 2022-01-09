c08f7632d04ba6650009f06958211395ad802daa
Jai A <jaiagr+gpg@pm.me>
2022-01-09 15:15:47 -0700

    Fix prettier error :(

--- nuxt.config.js
+++ nuxt.config.js
@@ -113,8 +113,7 @@ export default {
       },
       {
         rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap',
+        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap',
       },
     ],
     script: [],
