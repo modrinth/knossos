b9400e98f1c447da20da48e9690d01b9152bf7de
venashial <venashial.levo@aleeas.com>
2022-01-22 22:23:02 -0800

    Add following page

---
+++ src/routes/following.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import { t } from 'svelte-intl-precompile'
+  import Meta from '$components/utils/Meta.svelte'
+  import { following, updateSelf, loaded } from '$stores/self'
+  import { ago } from '$lib/ago'
+  import Button from '$components/elements/Button.svelte'
+  import IconHeart from 'virtual:icons/heroicons-outline/heart'
+  import { send } from '$lib/api'
+  import { onMount } from 'svelte'
+  import UpToDate from '$assets/images/illustrations/undraw_notify.svg'
+  import ProjectCard from "$components/elements/ProjectCard.svelte";
+  import IconCalendar from 'virtual:icons/lucide/calendar'
+
+  onMount(async () => {
+    await updateSelf()
+  })
+</script>
+
+<Meta title={$t('pages.following')}/>
+
+<div class="single-layout">
+  <div class="title-bar">
+    <h1>{$t('pages.following')}</h1>
+  </div>
+  {#if !$loaded}
+    Loading followed projects...
+  {:else if $following.length > 0}
+    {#each $following as project}
+        <ProjectCard {project}>
+          <div class="actions" slot="actions">
+            <Button label={$t('generic.actions.unfollow')} icon={IconHeart} on:click={async () => {
+              await send('DELETE', `project/${project.id}/follow`)
+              $following = $following.filter(followed => followed.id !== project.id)
+            }} />
+            <span class="stat">
+              <IconCalendar/>
+              {@html $t('stats.updated', {values: {ago: ago(project.updated)}})}
+            </span>
+          </div>
+        </ProjectCard>
+    {/each}
+  {:else}
+    <div class="illustration">
+      <UpToDate class="illustration__image" />
+      <p class="illustration__description">You are up to date!</p>
+    </div>
+  {/if}
+</div>
+
+<style lang="postcss">
+  .title-bar {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+
+    @media (width <= 400px) {
+      flex-direction: column;
+      align-items: flex-start;
+      grid-gap: 0.75rem;
+    }
+  }
+
+    .actions {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      grid-gap: 0.5rem;
+      justify-content: space-between;
+      height: 100%;
+    }
+</style>
--- src/stores/self.ts
+++ src/stores/self.ts
@@ -4,14 +4,18 @@ import { token, user } from '$stores/server';
 
 export const loaded = writable(false);
 
-export const follows = writable([]);
+export const following = writable([]);
 export const notifications = writable([]);
 
 export async function updateSelf(force = false): Promise<void> {
 	if ((!get(loaded) || force) && get(token)) {
 		const userValue = get(user);
 		await Promise.all([
-			follows.set((await send('GET', `user/${userValue.id}/follows`)) as []),
+			following.set(
+				((await send('GET', `user/${userValue.id}/follows`)) as []).sort((a, b) =>
+					a.title > b.title ? 1 : -1
+				)
+			),
 			notifications.set((await send('GET', `user/${userValue.id}/notifications`)) as []),
 		]);
 
