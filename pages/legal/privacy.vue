b678c1c7afed858fcd18a47853c3019b57d0b802
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-07-12 01:01:32 -0400

    Overhaul of the legal pages (rules, terms, privacy policy)
    Resolves MOD-90
    Resolves MOD-91

--- pages/legal/privacy.vue
+++ pages/legal/privacy.vue
@@ -3,39 +3,68 @@
     <div class="card">
       <h1>Privacy Policy</h1>
 
+      <h2>0. Boilerplate</h2>
+
       <p>
-        At Modrinth, accessible from https://modrinth.com, one of our main
-        priorities is the privacy of our visitors. This Privacy Policy document
-        contains types of information that is collected and recorded by Modrinth
-        and how we use it.
+        References in this document to "Modrinth", "we", "us", and "our" refer
+        to Rinth, Inc., a domestic corporation registered in the State of
+        Delaware. "Our website" refers to the website which you are browsing
+        right now, which can primarily be accessed through
+        <a href="https://modrinth.com">modrinth.com</a>.
+      </p>
+
+      <p>
+        Any claim related to Rinth, Inc. and our website shall be governed by
+        the laws of the United States and of the State of Delaware, without
+        regard to conflict of law.
+      </p>
+
+      <h2>1. About this Policy</h2>
+
+      <p>
+        At Modrinth, one of our main priorities is the privacy of our visitors.
+        This Privacy Policy document contains types of information that is
+        collected and recorded by Modrinth and how we use it.
+      </p>
+
+      <p>
+        This Privacy Policy applies only to our online activities and is valid
+        for visitors to our website with regards to the information that they
+        share and/or collect in Modrinth. This policy is not applicable to any
+        information collected offline or via channels other than this website.
       </p>
 
       <p>
         If you have additional questions or require more information about our
-        Privacy Policy, do not hesitate to contact us.
+        Privacy Policy, do not hesitate to contact us at admin@modrinth.com.
       </p>
 
-      <h2>General Data Protection Regulation (GDPR)</h2>
-      <p>We (Modrinth) are a Data Controller of your information.</p>
+      <h2>2. General Data Protection Regulation (GDPR)</h2>
 
       <p>
-        Rinth, Inc. legal basis for collecting and using the personal
-        information described in this Privacy Policy depends on the Personal
-        Information we collect and the specific context in which we collect the
-        information:
+        We are a
+        <a
+          href="https://ec.europa.eu/info/law/law-topic/data-protection/reform/rules-business-and-organisations/obligations/controller-processor/what-data-controller-or-data-processor_en"
+          >controller of your information</a
+        >.
+      </p>
+
+      <p>
+        Our legal basis for collecting and using the personal information
+        described in this Privacy Policy depends on the Personal Information we
+        collect and the specific context in which we collect the information:
       </p>
       <ul>
-        <li>Rinth, Inc. needs to perform a contract with you</li>
-        <li>You have given Rinth, Inc. permission to do so</li>
+        <li>We need to perform a contract with you</li>
+        <li>You have given us permission to do so</li>
         <li>
-          Processing your personal information is in Rinth, Inc. legitimate
-          interests
+          Processing your personal information is in our legitimate interests
         </li>
-        <li>Rinth, Inc. needs to comply with the law</li>
+        <li>We need to comply with the law</li>
       </ul>
 
       <p>
-        Rinth, Inc. will retain your personal information only for as long as is
+        We will retain your personal information only for as long as is
         necessary for the purposes set out in this Privacy Policy. We will
         retain and use your information to the extent necessary to comply with
         our legal obligations, resolve disputes, and enforce our policies.
@@ -44,8 +73,8 @@
       <p>
         If you are a resident of the European Economic Area (EEA), you have
         certain data protection rights. If you wish to be informed what Personal
-        Information we hold about you and if you want it to be removed from our
-        systems, please contact us.
+        Information we hold about you or if you want your Personal Information
+        to be removed from our systems, please contact us at admin@modrinth.com.
       </p>
 
       <p>
@@ -53,9 +82,11 @@
       </p>
       <ul>
         <li>
-          The right to access, update or to delete the information we have on
-          you. (please email admin@modrinth.com for data requests)
+          The right to access, update, or delete the information we have on you.
         </li>
+        <ul>
+          <li>To exercise this right, please email admin@modrinth.com.</li>
+        </ul>
         <li>The right of rectification.</li>
         <li>The right to object.</li>
         <li>The right of restriction.</li>
@@ -63,7 +94,7 @@
         <li>The right to withdraw consent</li>
       </ul>
 
-      <h2>Log Files</h2>
+      <h2>3. Log files</h2>
 
       <p>
         Modrinth follows a standard procedure of using log files. These files
@@ -77,7 +108,7 @@
         the website, and gathering demographic information.
       </p>
 
-      <h2>Cookies and Web Beacons</h2>
+      <h2>4. Cookies and web beacons</h2>
 
       <p>
         Like any other website, Modrinth uses 'cookies'. These cookies are used
@@ -94,12 +125,7 @@
         >.
       </p>
 
-      <h2>Privacy Policies</h2>
-
-      <P
-        >You may consult this list to find the Privacy Policy for each of the
-        advertising partners of Modrinth.</P
-      >
+      <h2>5. Additional privacy policies</h2>
 
       <p>
         Third-party ad servers or ad networks uses technologies like cookies,
@@ -116,53 +142,35 @@
         are used by third-party advertisers.
       </p>
 
-      <h2>Third Party Privacy Policies</h2>
-
       <p>
-        Modrinth's Privacy Policy does not apply to other advertisers or
-        websites. Thus, we are advising you to consult the respective Privacy
-        Policies of these third-party ad servers for more detailed information.
-        It may include their practices and instructions about how to opt-out of
-        certain options.
+        This Privacy Policy does not apply to other advertisers or websites.
+        Thus, we advise you to consult the respective Privacy Policies of any
+        third-party ad servers for more detailed information. It may include
+        their practices and instructions about how to opt-out of certain
+        options.
       </p>
 
       <p>
         You can choose to disable cookies through your individual browser
-        options. To know more detailed information about cookie management with
-        specific web browsers, it can be found at the browsers' respective
-        websites. What Are Cookies?
+        options. Please consult the guides available for your web browser to
+        find more detailed information about cookie management.
       </p>
 
-      <h2>Children's Information</h2>
+      <h2>6. Children's information</h2>
 
       <p>
-        Another part of our priority is adding protection for children while
-        using the internet. We encourage parents and guardians to observe,
-        participate in, and/or monitor and guide their online activity.
+        Another part of our goal is to add protection for children who are using
+        our Website. We encourage parents and guardians to observe, monitor, and
+        guide their children's online activity.
       </p>
 
       <p>
         Modrinth does not knowingly collect any Personal Identifiable
-        Information from children under the age of 13. If you think that your
-        child provided this kind of information on our website, we strongly
-        encourage you to contact us immediately and we will do our best efforts
-        to promptly remove such information from our records.
-      </p>
-
-      <h2>Online Privacy Policy Only</h2>
-
-      <p>
-        This Privacy Policy applies only to our online activities and is valid
-        for visitors to our website with regards to the information that they
-        shared and/or collect in Modrinth. This policy is not applicable to any
-        information collected offline or via channels other than this website.
-      </p>
-
-      <h2>Consent</h2>
-
-      <p>
-        By using our website, you hereby consent to our Privacy Policy and agree
-        to its Terms and Conditions.
+        Information (PII) from children under the age of 13. If you think that
+        your child provided this kind of information on our website, we strongly
+        encourage you to contact us immediately at admin@modrinth.com, after
+        which we will promptly remove any such information from our records on a
+        best-effort basis.
       </p>
     </div>
   </div>
@@ -178,7 +186,7 @@ export default {
         hid: 'description',
         name: 'description',
         content:
-          'The privacy policy of Modrinth, an open source modding platform. Modrinth currently supports Minecraft, including the forge and fabric mod loaders.',
+          'The privacy policy of Modrinth, an open source modding platform focused on Minecraft.',
       },
       {
         hid: 'apple-mobile-web-app-title',
