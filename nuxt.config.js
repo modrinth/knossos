6e8b7395c05d4d5837b07f559fe8009d8e7cd143
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-07-26 12:56:54 -0400

    Convert from google fonts to bunny fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -114,7 +114,7 @@ export default {
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap',
+        href: 'https://fonts.bunny.net/css2?family=Inter:wght@400;500;700;800&display=swap',
       },
       {
         rel: 'search',
