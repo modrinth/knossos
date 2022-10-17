711aa8e729e5573b619f746c009f8985f435ab84
GreenTriangle <71787934+m44ster@users.noreply.github.com>
2022-10-17 23:00:06 +0200

    fix prettier

--- pages/_type/_id.vue
+++ pages/_type/_id.vue
@@ -505,8 +505,9 @@
               >our documentation</a
             >
             which provides instructions on using
-            <a href="https://atlauncher.com/about" target="_blank">ATLauncher</a
-            > and <a href="https://multimc.org/" target="_blank">MultiMC</a>.
+            <a href="https://atlauncher.com/about" target="_blank">ATLauncher</a>
+            ·and·<a·href="https://multimc.org/"·target="_blank">MultiMC</a>.·Pack
+            creators
           </div>
           <Advertisement
             v-if="
