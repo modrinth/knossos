361954c9cbb825d69fc2eb351da57d9b0d15e85e
GreenTriangle <71787934+m44ster@users.noreply.github.com>
2022-10-17 23:07:38 +0200

    Update _id.vue

--- pages/_type/_id.vue
+++ pages/_type/_id.vue
@@ -506,7 +506,7 @@
             >
             which provides instructions on using
             <a href="https://atlauncher.com/about" target="_blank">ATLauncher</a>
-            ·and·<a·href="https://multimc.org/"·target="_blank">MultiMC</a>.·Pack
+             and <a href="https://multimc.org/" target="_blank">MultiMC</a>. Pack
             creators
           </div>
           <Advertisement
