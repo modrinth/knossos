43a82b8df118bfd89a2ff6ebcaef03acf91d8b2d
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-10-20 16:45:37 -0400

    Fix

--- pages/search.vue
+++ pages/search.vue
@@ -225,7 +225,7 @@
         with
         <a href="https://atlauncher.com/about" target="_blank">ATLauncher</a>
         , <a href="https://multimc.org/" target="_blank">MultiMC</a> and
-        <a href="https://prismlauncher.org" target="_blank">Prism launcher</a>.
+        <a href="https://prismlauncher.org" target="_blank">Prism Launcher</a>.
         Pack creators can reference our documentation on
         <a
           href="https://docs.modrinth.com/docs/modpacks/creating_modpacks/"
