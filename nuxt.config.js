8d3f12212163f40f47515a41d70500efaa5bfef5
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-11-12 11:56:02 -0500

    Revert "Change the base url back"
    This reverts commit c1da89fddb83776b39f626eab33c8dc67f8a75e4.

--- nuxt.config.js
+++ nuxt.config.js
@@ -223,7 +223,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'https://staging-api.modrinth.com/v2/',
+    baseURL: getApiUrl(),
     headers: {
       common: {
         Accept: 'application/json',
