ed7fe7a884530c816d29be3e3635c6210fa582d3
mineblock11 <93472213+mineblock11@users.noreply.github.com>
2022-11-28 11:10:17 +0000

    Projects page

--- pages/dashboard/projects.vue
+++ pages/dashboard/projects.vue
@@ -1,22 +1,258 @@
 <template>
   <div>
+    <Modal header="Bulk Edit" ref="modal">
+      <section class="card">
+        <p>You have selected the following projects:</p>
+        <ul>
+          <li v-for="project in selectedProjects" v-bind:key="project.id">
+            {{ project.title }}
+          </li>
+        </ul>
+        <p>Select a field to edit:</p>
+      </section>
+    </Modal>
     <section class="universal-card">
       <h2>Projects</h2>
+      <div class="metrics">
+        <div class="metric">
+          <div class="label">Total Projects</div>
+          <div class="value">
+            {{ projects.length }}
+          </div>
+        </div>
+        <div class="metric">
+          <div class="label">Versions Released</div>
+          <div class="value">
+            {{ versions.length }}
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="universal-card">
+      <h2>Management</h2>
+      <p>You can bulk edit projects by selecting them on the table.</p>
+      <button
+        class="iconified-button"
+        @click="$refs.modal.show()"
+        :disabled="selectedProjects.length === 0"
+      >
+        Bulk Edit
+      </button>
+      <br />
+      <div class="table-container">
+        <table>
+          <tr>
+            <th>
+              <Checkbox
+                @input="
+                  selectedProjects === projects
+                    ? (selectedProjects = [])
+                    : selectAllProjects()
+                "
+                :value="selectedProjects === projects"
+              />
+            </th>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Type</th>
+            <th>Status</th>
+            <th>Role</th>
+            <th></th>
+          </tr>
+          <tr v-for="project in projects" :key="project.id">
+            <td>
+              <Checkbox
+                :value="selectedProjects.includes(project)"
+                @input="
+                  selectedProjects.includes(project)
+                    ? (selectedProjects = selectedProjects.filter(
+                        (it) => it !== project
+                      ))
+                    : selectedProjects.push(project)
+                "
+              />
+            </td>
+            <td>{{ project.title }}</td>
+            <td>{{ project.id }}</td>
+            <td>{{ uppercaseString(project.project_type) }}</td>
+            <td>
+              <Badge
+                v-if="project.status && project.status !== 'approved'"
+                :type="project.status"
+                class="status"
+              />
+            </td>
+            <td>{{ project.role }}</td>
+            <td>
+              <nuxt-link :to="`/${project.project_type}/${project.id}/settings`"
+                ><SettingsIcon
+              /></nuxt-link>
+            </td>
+          </tr>
+        </table>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
+import Badge from '~/components/ui/Badge.vue'
+import Checkbox from '~/components/ui/Checkbox.vue'
+import Modal from '~/components/ui/Modal.vue'
+
+import SettingsIcon from '~/assets/images/utils/settings.svg?inline'
+
 export default {
-  components: {},
+  components: {
+    Badge,
+    SettingsIcon,
+    Checkbox,
+    Modal,
+  },
+  async asyncData(data) {
+    try {
+      const [unprocessedProjects] = (
+        await Promise.all([
+          data.$axios.get(
+            `user/${data.$auth.user.id}/projects`,
+            data.$defaultHeaders()
+          ),
+        ])
+      ).map((it) => it.data)
+
+      const projects = []
+      const versions = []
+
+      await unprocessedProjects.forEach(async (project) => {
+        const teamData = (
+          await data.$axios.get(
+            `project/${project.id}/members`,
+            data.$defaultHeaders()
+          )
+        ).data
+        teamData.forEach((member) => {
+          if (member.user.id !== data.$auth.user.id) return
+          project.role = member.role
+          projects.push(project)
+        })
+
+        const versionData = (
+          await data.$axios.get(
+            `project/${project.slug}/version`,
+            data.$defaultHeaders()
+          )
+        ).data
+        project.versions = versionData
+        versions.push(versionData)
+      })
+
+      return {
+        projects,
+        versions,
+      }
+    } catch (e) {
+      data.error({
+        statusCode: 404,
+        message: e,
+      })
+    }
+  },
   data() {
-    return {}
+    return {
+      selectedProjects: [],
+      showBulkEditModal: false,
+    }
+  },
+  methods: {
+    selectAllProjects() {
+      this.selectedProjects = this.projects
+    },
+    uppercaseString(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    },
   },
   fetch() {},
   head: {
     title: 'Projects - Modrinth',
   },
-  methods: {},
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.status {
+  margin-top: var(--spacing-card-xs);
+}
+
+.metrics {
+  display: grid;
+  grid-gap: var(--spacing-card-md);
+  grid-template-columns: repeat(auto-fit, minmax(12rem, 1fr));
+
+  .metric {
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+    justify-content: center;
+    background-color: var(--color-bg);
+    border-radius: var(--size-rounded-card);
+    padding: var(--spacing-card-lg);
+    gap: var(--spacing-card-md);
+
+    .label {
+      color: var(--color-heading);
+      font-weight: bold;
+      font-size: 1rem;
+    }
+
+    .value {
+      color: var(--color-text-dark);
+      font-weight: bold;
+      font-size: 2rem;
+    }
+  }
+}
+
+.table-container {
+  display: inline-block;
+  overflow: scroll;
+  width: 100%;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+
+tr:nth-child(even) {
+  background-color: var(--color-table-alternate-row);
+}
+
+th {
+  background-color: var(--color-bg);
+  color: white;
+}
+
+table {
+  font-size: var(--font-size-sm);
+  border-spacing: 0;
+  border-radius: var(--size-rounded-sm);
+  border-collapse: collapse;
+  width: 100%;
+}
+
+tr:first-child > :first-child {
+  border-top-left-radius: var(--size-rounded-sm);
+}
+tr:first-child > :last-child {
+  border-top-right-radius: var(--size-rounded-sm);
+}
+tr:last-child > :first-child {
+  border-bottom-left-radius: var(--size-rounded-sm);
+}
+tr:last-child > :last-child {
+  border-bottom-right-radius: var(--size-rounded-sm);
+}
+
+th,
+td {
+  padding: var(--spacing-card-md);
+  text-align: left;
+  box-shadow: -1px -1px var(--border-color);
+}
+</style>
