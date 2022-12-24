26096393df50c507036a5d1348a43e7a378488fb
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-12-23 19:01:34 -0500

    Fix issues geo discovered

--- pages/_type/_id/edit.vue
+++ pages/_type/_id/edit.vue
@@ -711,7 +711,9 @@ export default {
       (x) => x.short === trimmedLicenseId
     ) ?? { friendly: 'Custom', short: licenseId.replaceAll('LicenseRef-', '') }
     this.allowOrLater = licenseId.includes('-or-later')
-    this.nonSpdxLicense = licenseId.includes('LicenseRef-')
+    this.nonSpdxLicense =
+      licenseId.includes('LicenseRef-') &&
+      this.license.short !== 'All-Rights-Reserved'
 
     this.clientSideType =
       this.newProject.client_side.charAt(0) +
@@ -732,7 +734,7 @@ export default {
       id += this.license.short
 
       if (this.license.requiresOnlyOrLater)
-        id += this.allowOrLater ? 'or-later' : '-only'
+        id += this.allowOrLater ? '-or-later' : '-only'
 
       if (this.nonSpdxLicense) id.replaceAll(' ', '-')
 
