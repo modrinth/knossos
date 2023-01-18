c7512f1eaa63986792fb1432555467ed5a5d2c22
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2023-01-18 17:14:45 -0500

    Fix rules

--- pages/legal/rules.vue
+++ pages/legal/rules.vue
@@ -152,7 +152,8 @@
       <li>Content containing sexual or explicit material</li>
       <li>Content promoting or sharing harmful or hateful behavior</li>
       <li>
-        Content with a focus on items such as alcohol, tobacco, and other drugs
+        Content with a focus on substances (drugs) considered to be illicit
+        in most American jurisdictions
       </li>
       <li>Content with an excessive amount of profane language</li>
     </ul>
