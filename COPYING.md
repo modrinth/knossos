7f5a90f414c0a4930c7696d9f9fdead3ba66834a
Jai A <jaiagr+gpg@pm.me>
2023-02-16 14:30:52 -0700

    Update privacy policy

--- COPYING.md
+++ COPYING.md
@@ -5,16 +5,19 @@ The source code of the knossos repository is licensed under the GNU Affero Gener
 ## Modrinth logo
 
 Any files depicting the Modrinth branding, including the wrench-in-labyrinth logo, the landing image, and variations thereof, are licensed as follows:
+
 > All rights reserved. © 2020-2022 Rinth, Inc.
 
 This includes, but may not be limited to, the following files:
+
 - assets/images/404.svg
 - assets/images/logo.svg
-- components/brand/*
+- components/brand/\*
 - static/favicon.ico
 - static/favicon-light.ico
 
 ## External logos
 
 The following files are owned by their respective copyright holders and must be used within each of their Brand Guidelines:
-- assets/images/external/*
+
+- assets/images/external/\*
--- pages/legal/privacy.vue
+++ pages/legal/privacy.vue
@@ -1,196 +1,252 @@
 <template>
   <div>
     <h1>Privacy Policy</h1>
-
-    <p>
-      At Modrinth, accessible from https://modrinth.com, one of our main
-      priorities is the privacy of our visitors. This Privacy Policy document
-      contains types of information that is collected and recorded by Modrinth
-      and how we use it.
-    </p>
-
-    <p>
-      If you have additional questions or require more information about our
-      Privacy Policy, do not hesitate to contact us.
-    </p>
-
-    <h2>General Data Protection Regulation (GDPR)</h2>
-    <p>We (Modrinth) are a Data Controller of your information.</p>
-
-    <p>
-      Rinth, Inc. legal basis for collecting and using the personal information
-      described in this Privacy Policy depends on the Personal Information we
-      collect and the specific context in which we collect the information:
-    </p>
-    <ul>
-      <li>Rinth, Inc. needs to perform a contract with you</li>
-      <li>You have given Rinth, Inc. permission to do so</li>
-      <li>
-        Processing your personal information is in Rinth, Inc. legitimate
-        interests
-      </li>
-      <li>Rinth, Inc. needs to comply with the law</li>
-    </ul>
-
-    <p>
-      Rinth, Inc. will retain your personal information only for as long as is
-      necessary for the purposes set out in this Privacy Policy. We will retain
-      and use your information to the extent necessary to comply with our legal
-      obligations, resolve disputes, and enforce our policies.
-    </p>
-
-    <p>
-      If you are a resident of the European Economic Area (EEA), you have
-      certain data protection rights. If you wish to be informed what Personal
-      Information we hold about you and if you want it to be removed from our
-      systems, please contact us by email at
-      <a href="mailto:gdpr@modrinth.com" rel="noopener noreferrer nofollow">
-        gdpr@modrinth.com</a
-      >.
-    </p>
-
-    <p>
-      In certain circumstances, you have the following data protection rights:
-    </p>
-    <ul>
-      <li>
-        The right to access, update or to delete the information we have on you.
-      </li>
-      <li>The right of rectification.</li>
-      <li>The right to object.</li>
-      <li>The right of restriction.</li>
-      <li>The right to data portability.</li>
-      <li>The right to withdraw consent.</li>
-    </ul>
-
-    <h2>Log Files</h2>
-
-    <p>
-      Modrinth follows a standard procedure of using log files. These files log
-      visitors when they visit websites. All hosting companies do this and a
-      part of hosting services' analytics. The information collected by log
-      files include internet protocol (IP) addresses, browser type, Internet
-      Service Provider (ISP), date and time stamp, referring/exit pages, and
-      possibly the number of clicks. These are not linked to any information
-      that is personally identifiable. The purpose of the information is for
-      analyzing trends, administering the site, tracking users' movement on the
-      website, and gathering demographic information.
-    </p>
-
-    <h2>Cookies and Web Beacons</h2>
-
-    <p>
-      Like any other website, Modrinth uses 'cookies'. These cookies are used to
-      store information including visitors' preferences, and the pages on the
-      website that the visitor accessed or visited. The information is used to
-      optimize the users' experience by customizing our web page content based
-      on visitors' browser type and/or other information.
-    </p>
-
-    <p>
-      For more general information on cookies, please read
-      <a href="https://www.privacypolicies.com/etblog/cookies/"
-        >"What Are Cookies"</a
-      >.
-    </p>
-
-    <h2>Payouts Program</h2>
-
-    <p>
-      Modrinth allows creators to enroll in the Creator Monetization Program
-      (the "CMP"). By enrolling in the CMP, your PayPal or Venmo email
-      addresses, phone numbers, and usernames will be collected. This is
-      required to transfer the money to your account. You may remove this
-      information at any time by removing your enrollment in the dashboard or by
-      contacting gdpr@modrinth.com.
-    </p>
-
-    <h2>Privacy Policies</h2>
-
-    <P
-      >You may consult this list to find the Privacy Policy for each of the
-      advertising partners of Modrinth.</P
-    >
-
-    <p>
-      Third-party ad servers or ad networks uses technologies like cookies,
-      JavaScript, or Web Beacons that are used in their respective
-      advertisements and links that appear on Modrinth, which are sent directly
-      to users' browser. They automatically receive your IP address when this
-      occurs. These technologies are used to measure the effectiveness of their
-      advertising campaigns and/or to personalize the advertising content that
-      you see on websites that you visit.
-    </p>
-
-    <p>
-      Note that Modrinth has no access to or control over these cookies that are
-      used by third-party advertisers.
-    </p>
-
-    <h2>Third Party Privacy Policies</h2>
-
-    <p>
-      Modrinth's Privacy Policy does not apply to other advertisers or websites.
-      Thus, we are advising you to consult the respective Privacy Policies of
-      these third-party ad servers for more detailed information. It may include
-      their practices and instructions about how to opt-out of certain options.
-    </p>
-
-    <p>
-      You can choose to disable cookies through your individual browser options.
-      To know more detailed information about cookie management with specific
-      web browsers, it can be found at the browsers' respective websites.
-    </p>
-
-    <p>
-      In addition, anytime you click on links (including advertising banners),
-      which take you to third-party websites, you will be subject to the third
-      parties’ privacy policies. While we support the protection of our
-      customer’s privacy on the Internet, Modrinth expressly disclaims any and
-      all liability for the actions of third parties, including but without
-      limitation to actions relating to the use and/or disclosure of personal
-      information by third parties.
-    </p>
-
-    <p>
-      Modrinth makes use of external ads providers. These include EthicalAds.
-      You can read EthicalAds's privacy policy here and the data they collect:
-      <a href="https://www.ethicalads.io/privacy-policy/"
-        >https://www.ethicalads.io/privacy-policy/</a
-      >
-    </p>
-
-    <h2>Children's Information</h2>
-
-    <p>
-      Another part of our priority is adding protection for children while using
-      the internet. We encourage parents and guardians to observe, participate
-      in, and/or monitor and guide their online activity.
-    </p>
-
-    <p>
-      Modrinth does not knowingly collect any Personal Identifiable Information
-      from children under the age of 13. If you think that your child provided
-      this kind of information on our website, we strongly encourage you to
-      contact us immediately and we will do our best efforts to promptly remove
-      such information from our records.
-    </p>
-
-    <h2>Online Privacy Policy Only</h2>
-
-    <p>
-      This Privacy Policy applies only to our online activities and is valid for
-      visitors to our website with regards to the information that they shared
-      and/or collect in Modrinth. This policy is not applicable to any
-      information collected offline or via channels other than this website.
-    </p>
-
-    <h2>Consent</h2>
-
-    <p>
-      By using our website, you hereby consent to our Privacy Policy and agree
-      to its Terms and Conditions.
-    </p>
+    <div>
+      <p>Last edited: 02/16/2023</p>
+      <h2>Foreword</h2>
+      <p>
+        The following document was created as required by the European Union
+        General Data Protection Regulation (EU GDPR). You can find more
+        information about that regulation on
+        <a href="https://gdpr.eu/">gdpr.eu</a>.
+      </p>
+      <p>
+        <a href="http://modrinth.com">modrinth.com</a> is part of Rinth, Inc.
+        (”Us”, “We”, “Our”). This privacy policy explains how we collect data,
+        process it, and your rights relative to your data.
+      </p>
+      <p>
+        Rinth, Inc. is the data controller for data collected through
+        modrinth.com
+      </p>
+      <h2>What data do we collect?</h2>
+      <p>We collect the following data:</p>
+      <h3>User data</h3>
+      <p>When you create an account, we collect:</p>
+      <ul>
+        <li>Your email</li>
+      </ul>
+      <ul>
+        <li>Your username</li>
+      </ul>
+      <ul>
+        <li>Your display name</li>
+      </ul>
+      <ul>
+        <li>Your profile picture</li>
+      </ul>
+      <ul>
+        <li>Your GitHub ID</li>
+      </ul>
+      <p>
+        This data is used to identify you and display your profile. It will be
+        linked to your projects.
+      </p>
+      <h3>Download data</h3>
+      <p>When you download a file from us:</p>
+      <ul>
+        <li>Your IP address</li>
+      </ul>
+      <ul>
+        <li>Your user ID</li>
+      </ul>
+      <ul>
+        <li>The file downloaded</li>
+      </ul>
+      <ul>
+        <li>Your country</li>
+      </ul>
+      <ul>
+        <li>Some additional metadata about your connection (HTTP headers)</li>
+      </ul>
+      <p>
+        This data is used to prevent automated access to our files and deliver
+        statistics.
+      </p>
+      <h3>View data</h3>
+      <p>When you view a project:</p>
+      <ul>
+        <li>Your IP address</li>
+      </ul>
+      <ul>
+        <li>Your user ID</li>
+      </ul>
+      <ul>
+        <li>The project viewed</li>
+      </ul>
+      <ul>
+        <li>Your country</li>
+      </ul>
+      <ul>
+        <li>Some additional metadata about your connection (HTTP headers)</li>
+      </ul>
+      <p>
+        This data is used to prevent automated access to our service and deliver
+        statistics.
+      </p>
+      <h3>Payout data</h3>
+      <p>
+        When you sign up for our
+        <a href="https://blog.modrinth.com/p/creator-monetization-beta"
+          >payout program</a
+        >, we collect:
+      </p>
+      <ul>
+        <li>Your PayPal email address (if applicable)</li>
+      </ul>
+      <ul>
+        <li>Your Venmo username (if applicable)</li>
+      </ul>
+      <p>
+        This data is used to carry out payouts. It will be linked to your
+        transactions.
+      </p>
+      <h1>Data retention</h1>
+      <p>
+        Download data and View data are anonymized 24 months after being
+        recorded. All personal information will be removed from those records
+        during anonymization.
+      </p>
+      <p>
+        Data is retained indefinitely. We do not delete any data unless you
+        request it.
+      </p>
+      <h1>Third-party services</h1>
+      <p>
+        We use some third-party services to make
+        <a href="http://modrinth.com">modrinth.com</a> run. Please refer to
+        their website for more information:
+      </p>
+      <ul>
+        <li>
+          <a href="https://www.cloudflare.com/en-gb/gdpr/introduction/"
+            >Cloudflare</a
+          >
+        </li>
+      </ul>
+      <ul>
+        <li><a href="https://sentry.io/trust/privacy/">Sentry</a></li>
+      </ul>
+      <p>
+        Your data isn’t shared with any other third party. We do not sell any
+        data.
+      </p>
+      <h1>
+        <strong><strong>Data Governance</strong></strong>
+      </h1>
+      <p>
+        Database access is limited to the minimum amount of Rinth, Inc.
+        employees required to run the service.
+      </p>
+      <p>
+        Data is stored in a country part of the European Economic Area (EEA) and
+        encrypted at rest and in transit.
+      </p>
+      <h2>Marketing and advertising</h2>
+      <p>
+        We use anonymized statistics to conduct marketing and advertising
+        through <a href="https://adrinth.com/">Adrinth</a>.
+      </p>
+      <h2>Cookies</h2>
+      <p>
+        We use cookies to log you into your account and save your cosmetic
+        preferences.
+      </p>
+      <p>
+        Cookies are text files placed on your computer to collect standard
+        Internet information. For more information, please visit
+        <a href="https://allaboutcookies.org/">allaboutcookies.org</a>.
+      </p>
+      <p>
+        You can set your browser not to accept cookies, and the above website
+        tells you how to remove cookies from your browser. However, in a few
+        cases, some of our website features may not function as a result.
+      </p>
+      <h2>
+        <strong
+          ><strong
+            >Access, rectification, erasure, restriction, portability, and
+            objection</strong
+          ></strong
+        >
+      </h2>
+      <p>Every user is entitled to the following:</p>
+      <p>
+        <strong>The right to access</strong> – You have the right to request us
+        copies of your personal data. We may charge you a small fee for this
+        service.
+      </p>
+      <p>
+        <strong>The right to rectification</strong> – You have the right to
+        request that We correct any information you believe is inaccurate. You
+        also have the right to request Us to complete the information you
+        believe is incomplete.
+      </p>
+      <p>
+        <strong>The right to erasure</strong> – You have the right to request
+        that We erase your personal data, under certain conditions.
+      </p>
+      <p>
+        <strong>The right to restrict processing</strong> – You have the right
+        to request that We restrict the processing of your personal data, under
+        certain conditions.
+      </p>
+      <p>
+        <strong>The right to data portability</strong> – You have the right to
+        request that We transfer the data that we have collected to another
+        organization, or directly to you, under certain conditions.
+      </p>
+      <p>
+        <strong>The right to object to processing</strong> – You have the right
+        to object to Our processing of your personal data, under certain
+        conditions.
+      </p>
+      <p>
+        If you would like to exercise those rights, contact us at
+        <a href="mailto:privacy@modrinth.com">privacy@modrinth.com</a>. We may
+        ask you to verify your identity before proceeding and will respond to
+        your request within 30 days as required by law, or notify you of an
+        extended reply time.
+      </p>
+      <h2>
+        <strong><strong>Changes to the Privacy Policy</strong></strong>
+      </h2>
+      <p>
+        We keep this privacy policy under regular review and place any updates
+        on this web page. If we do this, we will post the changes on this page
+        and update the “Edited” date at the top of this page.
+      </p>
+      <h2>Contact</h2>
+      <p>
+        If you have any questions about this privacy policy or how we process
+        your data, contact us at
+        <a href="mailto:privacy@modrinth.com">privacy@modrinth.com</a> or write
+        us at:
+      </p>
+      <p>
+        2565 W Flint Street<br />
+        Chandler, AZ, USA 85224
+      </p>
+      <h2>
+        <strong
+          ><strong>How to contact the appropriate authority</strong></strong
+        >
+      </h2>
+      <p>
+        Should you wish to fill a complaint or if you feel like we haven’t
+        addressed your concerns or request, you make contact the
+        <a href="https://ico.org.uk/">Information Commissioner’s Office</a>
+        using their online form or by writing at:
+      </p>
+      <p>
+        Information Commissioner&#x27;s Office<br />
+        Wycliffe House<br />
+        Water Lane<br />
+        Wilmslow<br />
+        Cheshire<br />
+        SK9 5AF
+      </p>
+    </div>
   </div>
 </template>
 
