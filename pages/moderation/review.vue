3afe4b9165959dddd1d33a99e5ee8df13d1f4d53
Prospector <prospectordev@gmail.com>
2023-04-06 08:31:13 -0700

    Fix review age coloring

--- pages/moderation/review.vue
+++ pages/moderation/review.vue
@@ -9,24 +9,41 @@
     <section class="universal-card">
       <h2>Project queue</h2>
       <div class="input-group">
-        <Chips v-model="projectType" :items="projectTypes" :format-label="(x) => x === 'all' ? 'All' : $formatProjectType(x) + 's'" />
-        <button v-if="oldestFirst" class="iconified-button push-right" @click="oldestFirst = false"><SortDescIcon />Sorting by oldest</button>
-        <button v-else class="iconified-button push-right" @click="oldestFirst = true"><SortAscIcon />Sorting by newest</button>
+        <Chips
+          v-model="projectType"
+          :items="projectTypes"
+          :format-label="(x) => (x === 'all' ? 'All' : $formatProjectType(x) + 's')"
+        />
+        <button v-if="oldestFirst" class="iconified-button push-right" @click="oldestFirst = false">
+          <SortDescIcon />Sorting by oldest
+        </button>
+        <button v-else class="iconified-button push-right" @click="oldestFirst = true">
+          <SortAscIcon />Sorting by newest
+        </button>
       </div>
       <div
-        v-for="project in projects.sort((a, b) => {
+        v-for="project in projects
+          .sort((a, b) => {
             if (oldestFirst) {
               return b.age - a.age
             } else {
               return a.age - b.age
             }
-          }).filter((x) => projectType === 'all' || app.$getProjectTypeForUrl(x.project_type, x.loaders) === projectType)"
+          })
+          .filter(
+            (x) =>
+              projectType === 'all' ||
+              app.$getProjectTypeForUrl(x.project_type, x.loaders) === projectType
+          )"
         :key="`project-${project.id}`"
         class="universal-card recessed project"
       >
         <div class="title-row">
-          <nuxt-link :to="`/${project.inferred_project_type}/${project.slug}`" class="iconified-stacked-link project-title">
-            <Avatar :src="project.icon_url" size="xs" no-shadow raised/>
+          <nuxt-link
+            :to="`/${project.inferred_project_type}/${project.slug}`"
+            class="iconified-stacked-link project-title"
+          >
+            <Avatar :src="project.icon_url" size="xs" no-shadow raised />
             <span class="stacked">
               <span class="title">{{ project.title }}</span>
               <span>{{ $formatProjectType(project.inferred_project_type) }}</span>
@@ -34,22 +51,48 @@
           </nuxt-link>
           by
           <nuxt-link :to="`/user/${project.owner.username}`" class="iconified-link">
-            <Avatar :src="project.owner.avatar_url" circle size="xxs" raised/>
+            <Avatar :src="project.owner.avatar_url" circle size="xxs" raised />
             <span>{{ project.owner.username }}</span>
           </nuxt-link>
           is requesting to be
           <Badge :type="project.requested_status" />
         </div>
         <div class="input-group">
-          <nuxt-link :to="`/${project.inferred_project_type}/${project.slug}`" class="iconified-button raised-button"><EyeIcon /> View project</nuxt-link>
-          <button class="iconified-button brand-button" @click="setProjectStatus(project, project.requested_status ? project.requested_status : 'approved')"><CheckIcon /> Approve</button>
-          <button class="iconified-button danger-button" @click="setProjectStatus(project, 'withheld')"><EyeOffIcon /> Withhold</button>
-          <button class="iconified-button danger-button" @click="setProjectStatus(project, 'rejected')"><CrossIcon /> Reject</button>
+          <nuxt-link
+            :to="`/${project.inferred_project_type}/${project.slug}`"
+            class="iconified-button raised-button"
+            ><EyeIcon /> View project</nuxt-link
+          >
+          <button
+            class="iconified-button brand-button"
+            @click="
+              setProjectStatus(
+                project,
+                project.requested_status ? project.requested_status : 'approved'
+              )
+            "
+          >
+            <CheckIcon /> Approve
+          </button>
+          <button
+            class="iconified-button danger-button"
+            @click="setProjectStatus(project, 'withheld')"
+          >
+            <EyeOffIcon /> Withhold
+          </button>
+          <button
+            class="iconified-button danger-button"
+            @click="setProjectStatus(project, 'rejected')"
+          >
+            <CrossIcon /> Reject
+          </button>
         </div>
         <span v-if="project.queued" :class="`submitter-info ${project.age_warning}`">
           <WarningIcon v-if="project.age_warning" />
           Submitted for review
-          <span v-tooltip="$dayjs(project.queued).format('MMMM D, YYYY [at] h:mm A')">{{ fromNow(project.queued) }}</span>
+          <span v-tooltip="$dayjs(project.queued).format('MMMM D, YYYY [at] h:mm A')">{{
+            fromNow(project.queued)
+          }}</span>
         </span>
         <span v-else class="submitter-info"><UnknownIcon /> Unknown queue date</span>
       </div>
@@ -57,8 +100,8 @@
   </div>
 </template>
 <script setup>
-import Chips from "~/components/ui/Chips.vue";
-import Avatar from "~/components/ui/Avatar.vue";
+import Chips from '~/components/ui/Chips.vue'
+import Avatar from '~/components/ui/Avatar.vue'
 import UnknownIcon from '~/assets/images/utils/unknown.svg'
 import CheckIcon from '~/assets/images/utils/check.svg'
 import EyeOffIcon from '~/assets/images/utils/eye-off.svg'
@@ -67,7 +110,7 @@ import EyeIcon from '~/assets/images/utils/eye.svg'
 import SortAscIcon from '~/assets/images/utils/sort-asc.svg'
 import SortDescIcon from '~/assets/images/utils/sort-desc.svg'
 import WarningIcon from '~/assets/images/utils/issues.svg'
-import Badge from "~/components/ui/Badge.vue";
+import Badge from '~/components/ui/Badge.vue'
 import ModalModeration from '~/components/ui/ModalModeration'
 
 useHead({
@@ -86,12 +129,14 @@ const [rawMembers] = await Promise.all([
   useBaseFetch('teams?ids=' + JSON.stringify(teamIds), app.$defaultHeaders()),
 ])
 
-const now = app.$dayjs();
+const now = app.$dayjs()
 const time24h = 86400000
 
 rawProjects.map((project) => {
-  project.owner = rawMembers.flat().find((x) => x.team_id === project.team && x.role === 'Owner').user
-  project.age = project.queued ? (now - app.$dayjs(project.queued)) * 3 : Number.MAX_VALUE
+  project.owner = rawMembers
+    .flat()
+    .find((x) => x.team_id === project.team && x.role === 'Owner').user
+  project.age = project.queued ? now - app.$dayjs(project.queued) : Number.MAX_VALUE
   project.age_warning = ''
   if (project.age > time24h * 2) {
     project.age_warning = 'danger'
@@ -103,7 +148,7 @@ rawProjects.map((project) => {
 })
 
 const projectTypes = computed(() => {
-  const set = new Set();
+  const set = new Set()
   set.add('all')
 
   for (const project of rawProjects) {
