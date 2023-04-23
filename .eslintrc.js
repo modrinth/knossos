45a5b3e2422789a9ef9f20d20ef5c659ed84ae57
falseresync <falseresync@gmail.com>
2023-04-23 14:57:06 +0300

    Return Vue and TS ESLint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
     '@nuxtjs/eslint-config-typescript',
     'plugin:prettier/recommended', // Integrate Prettier into ESLint
     'prettier', // Disable unnecessary ESLint rules in the presence of Prettier
