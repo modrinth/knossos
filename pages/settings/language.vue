f037809ccbfda666a49a696167c67cc4d70a0175
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-06-18 21:55:29 +0200

    Move .visually-hidden to shared utils and add copyright notice

--- pages/settings/language.vue
+++ pages/settings/language.vue
@@ -352,22 +352,6 @@ function onSearchSubmit(e: KeyboardEvent) {
 </template>
 
 <style scoped lang="scss">
-.visually-hidden {
-  width: 1px !important;
-  height: 1px !important;
-  padding: 0 !important;
-  margin: -1px !important; // Fix for https://github.com/twbs/bootstrap/issues/25686
-  overflow: hidden !important;
-  clip: rect(0, 0, 0, 0) !important;
-  white-space: nowrap !important;
-  border: 0 !important;
-
-  // Fix for positioned table caption that could become anonymous cells
-  &:not(caption) {
-    position: absolute !important;
-  }
-}
-
 .languages-list {
   display: flex;
   flex-direction: column;
