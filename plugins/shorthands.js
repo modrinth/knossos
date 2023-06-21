2779ae5a43a532cb69bf3ac327062ed300287d9c
Prospector <prospectordev@gmail.com>
2023-06-20 20:14:34 -0700

    plugin and data pack too

--- plugins/shorthands.js
+++ plugins/shorthands.js
@@ -110,6 +110,8 @@ export default defineNuxtPlugin((nuxtApp) => {
         return 'mod and plugin'
       } else if (isMod && isDataPack) {
         return 'mod and data pack'
+      } else if (isPlugin && isDataPack) {
+        return 'plugin and data pack'
       } else if (isDataPack) {
         return 'data pack'
       } else if (isPlugin) {
