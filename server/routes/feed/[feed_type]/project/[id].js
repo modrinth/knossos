66ace378a648972db537993d538c2a042671edd9
Basique <basiqueevangelist@yandex.ru>
2023-07-01 03:35:15 +0300

    Spruce up project updates feed

--- server/routes/feed/[feed_type]/project/[id].js
+++ server/routes/feed/[feed_type]/project/[id].js
@@ -1,6 +1,10 @@
 import { Feed } from 'feed'
 import { renderString, escapeXmlAttr } from '~/helpers/parse'
 
+const capitalizeString = (name) => {
+  return name ? name.charAt(0).toUpperCase() + name.slice(1) : name
+}
+
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
   const API_URL = config.apiBaseUrl
@@ -16,62 +20,79 @@ export default defineEventHandler(async (event) => {
     featuredImage = featuredImage.url
   }
 
+  const slugOrId = projectInformation.slug ?? projectInformation.id
+  const projectUrl = WEBSITE_URL + `/${projectInformation.project_type}/${slugOrId}`
+
   const feed = new Feed({
     title: projectInformation.title,
-    id: WEBSITE_URL + `/${projectInformation.project_type}/${projectInformation.id}`,
-    description:
-      `${projectInformation.title} is a ${projectInformation.project_type} with ${
-        projectInformation.downloads
-      } download${projectInformation.downloads > 1 ? 's' : ''}` +
-      `${
-        projectInformation.followers > 0
-          ? 'and ' + projectInformation.followers + ' follower' + projectInformation.followers > 1
-            ? 's'
-            : ''
-          : ''
-      } that is available on Modrinth, an open-source platform to host mods, modpacks, shaders, resource packs, plugins and datapacks.`,
+    id: projectUrl,
+    description: projectInformation.description,
     feedLinks: {
-      json: WEBSITE_URL + `/feed/json/project/${projectInformation.id}`,
-      atom: WEBSITE_URL + `/feed/atom/project/${projectInformation.id}`,
-      rss: WEBSITE_URL + `/feed/rss/project/${projectInformation.id}`,
+      json: WEBSITE_URL + `/feed/json/project/${slugOrId}`,
+      atom: WEBSITE_URL + `/feed/atom/project/${slugOrId}`,
+      rss: WEBSITE_URL + `/feed/rss/project/${slugOrId}`,
     },
     generator: 'Modrinth',
-    link: WEBSITE_URL + `/${projectInformation.project_type}/${projectInformation.id}`,
+    link: projectUrl,
     language: 'en',
     updated: new Date(projectInformation.updated),
     favicon: projectInformation.icon_url ?? 'https://cdn.modrinth.com/placeholder.png',
     image: featuredImage ?? undefined,
   })
 
-  projectVersions.forEach((version) => {
-    feed.addItem({
-      title: `New Version Released: ${version.name}`,
-      id:
-        WEBSITE_URL +
-        `/${projectInformation.project_type}/${projectInformation.id}/version/${version.id}`,
-      link:
-        WEBSITE_URL +
-        `/${projectInformation.project_type}/${projectInformation.id}/version/${version.id}`,
-      content: escapeXmlAttr(
-        `This version is for ${version.loaders.join(
-          ', '
-        )} and works on the following Minecraft versions: ${version.game_versions.join(', ')}<br>` +
-          // Check for changelog length being greater than 1 to ensure no blank changelog section.
-          `<h3>Changelog</h3>${renderString(
-            version.changelog.length > 1 ? version.changelog : 'No changelog was specified.'
-          )}`
-      ),
-      author: [
-        ...projectTeam.map((member) => {
-          return {
-            name: member.user.username,
-            link: WEBSITE_URL + `/user/${member.user.id}`,
-          }
-        }),
-      ],
-      date: new Date(version.date_published),
+  for (const category of projectInformation.categories) {
+    feed.addCategory(category)
+  }
+
+  for (const member of projectTeam) {
+    feed.addContributor({
+      name: member.user.username,
+      link: WEBSITE_URL + `/user/${member.user.username}`,
     })
-  })
+  }
+
+  for (const version of projectVersions) {
+    /**
+     * @type {Array<import('feed/lib/typings').Category>}
+     */
+    const categories = [...version.loaders, ...version.game_versions]
+      .map(x => {
+        return {
+          name: capitalizeString(x),
+          term: x
+        }
+      })
+
+    let author = projectTeam
+      .find(member => member.user.id == version.author_id)
+      ?.user
+      ?? await $fetch(API_URL + 'user/' + version.author_id)
+    
+    feed.addItem({
+      title: `${version.name}`,
+      id:
+        `${projectUrl}/version/${version.id}`,
+      link:
+        `${projectUrl}/version/${version.id}`,
+      content:
+        `<p><b>Loaders: ${version.loaders.map(capitalizeString).join(
+          ', '
+        )}<br> Minecraft versions: ${version.game_versions.join(', ')}</b></p>` +
+          // Check for changelog length being greater than 1 to ensure no blank changelog section.
+        renderString(
+          version.changelog.length > 1 ? version.changelog : 'No changelog was specified.'
+        )
+      ,
+      author: [
+        {
+          name: author.username,
+          link: WEBSITE_URL + `/user/${author.username}`,
+        }
+      ],
+      category: categories,
+      date: new Date(version.date_published)
+    })
+  }
 
   switch (event.context.params.feed_type.toLowerCase()) {
     case 'rss':
@@ -84,7 +105,7 @@ export default defineEventHandler(async (event) => {
       setResponseHeader(event, 'Content-Type', 'application/feed+json')
       return feed.json1()
     default:
-      setResponseStatus(event, 500)
+      setResponseStatus(event, 400)
       return 'Invalid Feed Type'
   }
 })
