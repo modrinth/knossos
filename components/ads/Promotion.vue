9ea17b003ea983a4b7c2f1c9d32c4d238025f44a
Jai A <jaiagr+gpg@pm.me>
2023-07-21 14:32:25 -0700

    Update advertisement

--- components/ads/Promotion.vue
+++ components/ads/Promotion.vue
@@ -1,71 +1,63 @@
 <template>
-  <div class="MYYLVTXBPUVWMLVBPVSDLHADDRYFBF">
-    <div class="MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-0">
-      <div class="MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-1">
-        <div class="MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-2">
+  <div class="BDVE3s9MFQttmbMUeYup">
+    <div class="BDVE3s9MFQttmbMUeYup-0">
+      <div class="BDVE3s9MFQttmbMUeYup-1">
+        <div class="BDVE3s9MFQttmbMUeYup-2">
           <a
             href="https://exaroton.com/?utm_source=modrinth&utm_medium=text&utm_campaign=host&utm_content=top"
             rel="noopener nofollow sponsored"
             target="_blank"
           >
-            <LightIcon
-              v-if="colorMode.value === 'light'"
+            <img
+              src="~/assets/images/external/mtx-logo.png"
               class="MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-3"
             />
-            <DarkIcon v-else class="MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-3" />
             <span>
-              <span> Host your Minecraft server on </span>
-              <strong>exaroton</strong>
-              <span> - only pay while the server is running - billed per second. </span>
+              <span> Increase your server playtime through </span>
+              <strong>Matrix</strong>
+              <span> - Increase your revenue or we’ll pay you ourselves. </span>
             </span>
           </a>
         </div>
       </div>
-      <div class="MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-4">
+      <div class="BDVE3s9MFQttmbMUeYup-4">
         <a rel="noopener sponsored" target="_blank" href="https://adrinth.com"> Ads via Adrinth </a>
       </div>
     </div>
   </div>
 </template>
-<script setup>
-import LightIcon from '~/assets/images/external/exaroton-light.svg'
-import DarkIcon from '~/assets/images/external/exaroton-dark.svg'
-
-const colorMode = useTheme()
-</script>
-
 <style>
-.MYYLVTXBPUVWMLVBPVSDLHADDRYFBF {
+.BDVE3s9MFQttmbMUeYup {
   position: relative;
   margin-bottom: var(--spacing-card-md);
   background: var(--color-ad);
   border: 3px solid var(--color-ad-raised);
   border-radius: var(--size-rounded-card);
 }
-.MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-0 {
+.BDVE3s9MFQttmbMUeYup-0 {
   font-size: 14px;
   line-height: 1.3em;
 }
-.MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-1 {
+.BDVE3s9MFQttmbMUeYup-1 {
   color: var(--color-text);
   padding: 1em;
   text-align: left;
 }
-.MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-2 a {
+.BDVE3s9MFQttmbMUeYup-2 a {
   display: flex;
   align-items: center;
   gap: 0.5rem;
 }
-.MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-2 a b,
-.MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-2 a strong {
+.BDVE3s9MFQttmbMUeYup-2 a b,
+.BDVE3s9MFQttmbMUeYup-2 a strong {
   color: #088cdb;
 }
-.MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-3 {
+.BDVE3s9MFQttmbMUeYup-3 {
   padding-top: 1px;
   height: 1.2rem;
   width: auto;
 }
-.MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-4 a {
+.BDVE3s9MFQttmbMUeYup-4 a {
   position: absolute;
   bottom: -2px;
   right: -2px;
@@ -84,10 +76,10 @@ const colorMode = useTheme()
   gap: 0.5rem;
 }
 @media screen and (max-width: 800px) {
-  .MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-2 {
+  .BDVE3s9MFQttmbMUeYup-2 {
     margin-bottom: 0.5rem;
   }
-  .MYYLVTXBPUVWMLVBPVSDLHADDRYFBF-2 a {
+  .BDVE3s9MFQttmbMUeYup-2 a {
     align-items: flex-start;
     flex-direction: column;
   }
