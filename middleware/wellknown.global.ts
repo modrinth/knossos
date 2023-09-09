1aada79b24988e11ac5864361ba8c277613313b7
Mysterious_Dev <mysteriousdevs@gmail.com>
2023-09-09 18:31:34 +0200

    Fiw lint error

--- middleware/wellknown.global.ts
+++ middleware/wellknown.global.ts
@@ -1,4 +1,4 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware((to) => {
   if (to.path === '/.well-known/change-password') {
     return navigateTo('/settings/account')
   }
