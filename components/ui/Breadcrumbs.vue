0b989e7da91d72d6819e3224115d0dad7aadfcde
Mysterious_Dev <mysteriousdevs@gmail.com>
2023-10-03 08:58:10 +0200

    Fix lint error

--- components/ui/Breadcrumbs.vue
+++ components/ui/Breadcrumbs.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script setup>
-import { ChevronRightIcon  } from 'omorphia'
+import { ChevronRightIcon } from 'omorphia'
 
 defineProps({
   linkStack: {
--- components/ui/CopyCode.vue
+++ components/ui/CopyCode.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script setup>
-
 import { CheckIcon, ClipboardCopyIcon } from 'omorphia'
 
 const copied = ref(false)
