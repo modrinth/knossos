ac985568d9c71b78694e84ad8e63e8befc28ad56
Mysterious_Dev <mysteriousdevs@gmail.com>
2023-10-07 11:44:17 +0200

    Fixed 404 logo not being displayed every time

--- error.vue
+++ error.vue
@@ -2,7 +2,7 @@
   <NuxtLayout>
     <div class="main">
       <div class="error">
-        <Logo404 v-if="error.statusCode === '404'" />
+        <Logo404 v-if="error.statusCode === 404" />
         <h1 v-else>An error occurred!</h1>
         <p>{{ error.message }}</p>
         <div class="button-group">
