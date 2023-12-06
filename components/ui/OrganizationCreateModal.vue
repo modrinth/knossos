9d2bb1e555efec05f746bd860654eb6bb9d4a6cf
Jai A <jaiagr+gpg@pm.me>
2023-12-05 19:54:44 -0700

    [WIP] Transfer organizations to own branch

---
+++ components/ui/OrganizationCreateModal.vue
@@ -0,0 +1,110 @@
+<template>
+  <Modal ref="modal" header="Create an organization">
+    <div class="universal-modal modal-creation universal-labels">
+      <div class="markdown-body">
+        <p>
+          New organizations are created with you as the owner and can be found under your profile
+          page.
+        </p>
+      </div>
+      <label for="name">
+        <span class="label__title">Name<span class="required">*</span></span>
+      </label>
+      <input
+        id="name"
+        v-model="name"
+        type="text"
+        maxlength="64"
+        :placeholder="`Enter organization name...`"
+        autocomplete="off"
+      />
+      <label for="additional-information">
+        <span class="label__title">Summary<span class="required">*</span></span>
+        <span class="label__description">This appears on your organization's page.</span>
+      </label>
+      <div class="textarea-wrapper">
+        <textarea id="additional-information" v-model="description" maxlength="256" />
+      </div>
+      <div class="push-right input-group">
+        <Button @click="modal.hide()">
+          <CrossIcon />
+          Cancel
+        </Button>
+        <Button color="primary" @click="createProject">
+          <CheckIcon />
+          Continue
+        </Button>
+      </div>
+    </div>
+  </Modal>
+</template>
+<script setup>
+import { XIcon as CrossIcon, CheckIcon, Modal, Button } from 'omorphia'
+
+const router = useRouter()
+
+const name = ref('')
+const description = ref('')
+
+const modal = ref()
+
+async function createProject() {
+  startLoading()
+  try {
+    const value = {
+      name: name.value.trim().replace(/ +/g, ''),
+      description: description.value.trim(),
+    }
+
+    const result = await useBaseFetch('organization', {
+      method: 'POST',
+      body: JSON.stringify(value),
+      apiVersion: 3,
+    })
+
+    modal.value.hide()
+
+    console.log(result)
+    await router.push(`/organization/${result.id}`)
+  } catch (err) {
+    console.error(err)
+    addNotification({
+      group: 'main',
+      title: 'An error occurred',
+      text: err.data.description,
+      type: 'error',
+    })
+  }
+  stopLoading()
+}
+function show() {
+  name.value = ''
+  description.value = ''
+  modal.value.show()
+}
+
+defineExpose({
+  show,
+})
+</script>
+
+<style scoped lang="scss">
+.modal-creation {
+  input {
+    width: 20rem;
+    max-width: 100%;
+  }
+
+  .text-input-wrapper {
+    width: 100%;
+  }
+
+  textarea {
+    min-height: 5rem;
+  }
+
+  .input-group {
+    margin-top: var(--gap-md);
+  }
+}
+</style>
---
+++ pages/organization/[id]/index.vue
@@ -0,0 +1,117 @@
+<script setup>
+import { Card, SearchIcon, Button, XIcon, Avatar, Promotion, ProjectCard } from 'omorphia'
+const cosmetics = useCosmetics()
+const tags = useTags()
+const auth = await useAuth()
+const user = await useUser()
+const props = defineProps({
+  organization: {
+    type: Object,
+    default() {
+      return {}
+    },
+  },
+  projects: {
+    type: Array,
+    default() {
+      return []
+    },
+  },
+  typeFilter: {
+    type: String,
+    default: '',
+  },
+})
+const inputText = ref('')
+const selectedFilters = ref([])
+selectedFilters.value.push(
+  ...props.projects.map((p) => p.project_type).filter((v, i, a) => a.indexOf(v) === i)
+)
+</script>
+
+<template>
+  <div class="normal-page__content">
+    <Promotion />
+    <div class="search-row">
+      <div class="iconified-input">
+        <SearchIcon />
+        <input id="search-input" v-model="inputText" type="text" placeholder="Search projects..." />
+        <Button class="r-btn" :class="inputText ? '' : 'empty'" @click="() => (inputText = '')">
+          <XIcon />
+        </Button>
+      </div>
+    </div>
+    <div class="project-list display-mode--list">
+      <ProjectCard
+        v-for="project in projects
+          .filter((p) => typeFilter === p.project_type || typeFilter === 'all')
+          .filter((p) => p.title.toLowerCase().includes(inputText.toLowerCase()))"
+        :id="project.slug || project.id"
+        :key="project.id"
+        :name="project.title"
+        :display="cosmetics.searchDisplayMode.user"
+        :featured-image="
+          project.gallery
+            .slice()
+            .sort((a, b) => b.featured - a.featured)
+            .map((x) => x.url)[0]
+        "
+        :description="project.description"
+        :created-at="project.published"
+        :updated-at="project.updated"
+        :downloads="project.downloads.toString()"
+        :follows="project.followers.toString()"
+        :icon-url="project.icon_url"
+        :categories="project.categories"
+        :client-side="project.client_side"
+        :server-side="project.server_side"
+        :status="
+          auth.user && (auth.user.id === user.id || tags.staffRoles.includes(auth.user.role))
+            ? project.status
+            : null
+        "
+        :type="project.project_type"
+        :color="project.color"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.title-and-link {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h3 {
+    margin: 0;
+  }
+  a {
+    display: flex;
+    align-items: center;
+    gap: var(--gap-xs);
+    color: var(--color-blue);
+  }
+}
+.project-overview {
+  gap: var(--gap-md);
+  padding: var(--gap-xl);
+  .project-card {
+    padding: 0;
+    border-radius: 0;
+    background-color: transparent;
+    box-shadow: none;
+    :deep(.title) {
+      font-size: var(--font-size-nm) !important;
+    }
+  }
+}
+.popout-heading {
+  padding: var(--gap-sm) var(--gap-md);
+  margin: 0;
+  font-size: var(--font-size-md);
+  color: var(--color-text);
+}
+.popout-checkbox {
+  padding: var(--gap-sm) var(--gap-md);
+}
+</style>
