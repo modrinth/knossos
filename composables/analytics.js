c4f976937f861df67947f0cba6ea1db7b2535595
Carter <safe@fea.st>
2023-12-08 20:13:25 -0800

    refactor analytics into reusables

---
+++ composables/analytics.js
@@ -0,0 +1,67 @@
+import dayjs from 'dayjs'
+import { all } from 'iso-3166-1'
+
+const countries = all()
+  .map((entry) => ({
+    [entry.alpha2]: entry.country,
+  }))
+  .reduce((acc, cur) => ({ ...acc, ...cur }), {})
+
+export const countryCodeToName = (code) => {
+  if (code === 'TW') return 'Taiwan'
+  if (code === 'XX') return 'Unknown'
+  if (countries[code]) {
+    return countries[code]
+  }
+  return code
+}
+
+export const formatTimestamp = (timestamp) => {
+  return dayjs.unix(timestamp).format('YYYY-MM-DD')
+}
+
+export const formatPercent = (value, sum) => {
+  console.log(value, sum)
+  return `${((value / sum) * 100).toFixed(2)}%`
+}
+
+export const processAnalytics = (category, projectId, labelFn, sortFn, mapFn, chartName) => {
+  const projectData = category?.[projectId]
+  if (!projectData) {
+    return null
+  }
+
+  const entries = Object.entries(projectData)
+  const sortedEntries = sortFn ? entries.sort(sortFn) : entries
+
+  const mappedEntries = mapFn ? sortedEntries.map(mapFn) : sortedEntries
+
+  return {
+    sum: mappedEntries.reduce((acc, cur) => acc + cur[1], 0),
+    len: mappedEntries.length,
+    chart: {
+      labels: mappedEntries.map(([ts]) => labelFn(ts)),
+      data: [
+        {
+          name: chartName,
+          data: mappedEntries.map(([_, value]) => value),
+        },
+      ],
+    },
+  }
+}
+
+export const processAnalyticsByCountry = (category, projectId, sortFn) => {
+  const projectData = category?.[projectId]
+  if (!projectData) {
+    return null
+  }
+
+  const entries = Object.entries(projectData).sort(sortFn)
+
+  return {
+    sum: entries.reduce((acc, cur) => acc + cur[1], 0),
+    len: entries.length,
+    data: entries,
+  }
+}
