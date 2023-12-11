fca37d2c70976bc1492ccb6159797e5ba960c74c
Carter <safe@fea.st>
2023-12-11 16:46:25 -0700

    hook up collections page

--- pages/creations/collections.vue
+++ pages/creations/collections.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <CollectionCreateModal ref="modal_creation" />
     <h2>Collections</h2>
     <div class="search-row">
       <div class="iconified-input">
@@ -10,9 +11,25 @@
           <XIcon />
         </Button>
       </div>
-      <Button color="primary"> <PlusIcon /> Create new </Button>
+      <Button color="primary" @click="$refs.modal_creation.show()">
+        <PlusIcon /> Create new
+      </Button>
     </div>
     <div class="collections-grid">
+      <nuxt-link
+        v-for="collection in orderedCollections"
+        :to="`/collection/${collection.id}`"
+        class="card collection"
+      >
+        <Avatar :src="collection.icon_url" class="icon" />
+        <div class="details">
+          <span class="title">{{ collection.name }}</span>
+          <span class="description">
+            {{ collection.description }}
+          </span>
+          <span class="stats"><BoxIcon /> {{ collection.projects?.length || 0 }} projects</span>
+        </div>
+      </nuxt-link>
       <nuxt-link
         v-if="'followed projects'.includes(filterQuery)"
         :to="`/collection/following`"
@@ -35,6 +52,7 @@
 </template>
 <script setup>
 import { Avatar, BoxIcon, SearchIcon, XIcon, Button, PlusIcon } from 'omorphia'
+import CollectionCreateModal from '~/components/ui/CollectionCreateModal.vue'
 
 definePageMeta({
   middleware: 'auth',
@@ -45,9 +63,23 @@ useHead({
 })
 
 const user = await useUser()
+const auth = await useAuth()
 if (process.client) {
   await initUserFollows()
 }
+
+const { data: collections } = await useAsyncData(`user/${auth.value.user.id}/collections`, () =>
+  useBaseFetch(`user/${auth.value.user.id}/collections`, { apiVersion: 3 })
+)
+
+const orderedCollections = computed(() => {
+  if (!collections.value) return []
+  return collections.value.sort((a, b) => {
+    const aUpdated = new Date(a.updated)
+    const bUpdated = new Date(b.updated)
+    return bUpdated - aUpdated
+  })
+})
 
 const filterQuery = ref('')
 </script>
