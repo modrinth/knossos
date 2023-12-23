8d1d1fe2f9b3c17932f035bc55a2a3a8886cfd24
Carter <safe@fea.st>
2023-12-22 23:49:16 -0800

    random rendering issue

--- pages/[type]/[id]/index.vue
+++ pages/[type]/[id]/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="markdown-body card" v-html="renderHighlightedString(project.body)" />
+  <div class="markdown-body card" v-html="renderHighlightedString(project.body || '')" />
 </template>
 
 <script>
