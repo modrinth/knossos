6d5f90b81762a8e6320aa27d84a3da523a0fa07e
Jai A <jaiagr+gpg@pm.me>
2024-02-06 20:17:22 -0700

    remove explainer

--- components/ui/ModerationChecklist.vue
+++ components/ui/ModerationChecklist.vue
@@ -506,13 +506,6 @@ Per section 5.4 of [Modrinth's Content Rules](https://modrinth.com/legal/rules#m
           resultingMessage: `## Unreachable Links
 Per section 5.4 of [Modrinth's Content Rules](https://modrinth.com/legal/rules#miscellaneous) all links must lead to correctly labeled publicly available resources that are directly related to your project.
 Currently, your Source link directs to a Page Not Found error, likely because your repository is private, make sure to make your repository public before resubmitting your project!`,
-          fillers: [
-            {
-              id: 'EXPLAINER',
-              question: 'Please elaborate on how the author can improve their description.',
-              large: true,
-            },
-          ],
         },
         {
           name: 'Not accessible (other)',
