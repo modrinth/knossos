b6be7c8e11a4d58bee7cea35ca524f8552964bdf
Prospector <prospectordev@gmail.com>
2024-03-31 22:27:56 -0700

    new settings work

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,36 +1,45 @@
 <template>
   <div>
+    <MessageBanner v-if="cosmetics.developerMode" message-type="warning" class="developer-message">
+      <CodeIcon /> <strong>Developer mode</strong> is active. This will allow you to view the
+      internal IDs of various things throughout Modrinth that may be helpful if you're a developer
+      using the Modrinth API. Click on the Modrinth logo at the bottom of the page 5 times to toggle
+      developer mode.
+      <Button :action="() => disableDeveloperMode()"> Deactivate developer mode </Button>
+    </MessageBanner>
     <section class="universal-card">
-      <h2>Themes</h2>
-      <div class="adjacent-input">
-        <label for="theme-selector">
-          <span class="label__title">Color theme</span>
-          <span class="label__description">Change the global site color theme.</span>
-        </label>
-        <div>
-          <Multiselect
-            id="theme-selector"
-            v-model="$colorMode.preference"
-            :options="['system', 'light', 'dark', 'oled']"
-            :custom-label="
-              (value) =>
-                value === 'oled' ? 'OLED' : value.charAt(0).toUpperCase() + value.slice(1)
-            "
-            :searchable="false"
-            :close-on-select="true"
-            :show-labels="false"
-            :allow-empty="false"
-            @update:model-value="(value) => updateTheme(value, true)"
-          />
-        </div>
+      <h2>Color theme</h2>
+      <div class="theme-options">
+        <button
+          v-for="option in options"
+          :key="option"
+          class="theme-option button-base"
+          :class="{ selected: theme.preference === option }"
+          @click="() => updateColorTheme(option)"
+        >
+          <div class="preview" :class="`${option === 'system' ? systemTheme : option}-mode`">
+            <div class="example-card card card">
+              <div class="example-icon"></div>
+              <div class="example-text-1"></div>
+              <div class="example-text-2"></div>
+            </div>
+          </div>
+          <div class="label">
+            <RadioButtonChecked v-if="theme.preference === option" class="radio" />
+            <RadioButtonIcon v-else class="radio" />
+            {{ formatOption(option) }}
+          </div>
+        </button>
       </div>
-
+    </section>
+    <section class="universal-card">
+      <h2>Sidebar mode</h2>
       <div class="adjacent-input small">
         <label for="search-layout-toggle">
           <span class="label__title">Search sidebar on the right</span>
-          <span class="label__description"
-            >Enabling this will put the search page's filters sidebar on the right side.</span
-          >
+          <span class="label__description">
+            Enabling this will put the search page's filters sidebar on the right side.
+          </span>
         </label>
         <input
           id="search-layout-toggle"
@@ -139,7 +148,9 @@
 
 <script setup>
 import { Multiselect } from 'vue-multiselect'
+import { CodeIcon, Button, RadioButtonIcon, RadioButtonChecked } from 'omorphia'
 import { formatProjectType } from '~/plugins/shorthands.js'
+import MessageBanner from '~/components/ui/MessageBanner.vue'
 
 useHead({
   title: 'Display settings - Modrinth',
@@ -147,6 +158,55 @@ useHead({
 
 const cosmetics = useCosmetics()
 const tags = useTags()
+
+const systemTheme = ref('light')
+
+const theme = useTheme()
+
+const options = computed(() => {
+  const options = ['system', 'light', 'dark', 'oled']
+  if (cosmetics.value.developerMode || theme.value.preference === 'retro') {
+    options.push('retro')
+  }
+  return options
+})
+
+function formatOption(value) {
+  if (value === 'oled') {
+    return 'OLED'
+  } else if (value === 'system') {
+    return 'Sync with system'
+  }
+  return value.charAt(0).toUpperCase() + value.slice(1)
+}
+
+onMounted(() => {
+  const colorSchemeQueryList = window.matchMedia('(prefers-color-scheme: light)')
+  if (colorSchemeQueryList.matches) {
+    systemTheme.value = 'light'
+  } else {
+    systemTheme.value = 'dark'
+  }
+})
+
+function updateColorTheme(value) {
+  if (['dark', 'oled', 'retro'].includes(value)) {
+    cosmetics.value.preferredDarkTheme = value
+    saveCosmetics()
+  }
+  updateTheme(value, true)
+}
+
+function disableDeveloperMode() {
+  cosmetics.value.developerMode = !cosmetics.value.developerMode
+  saveCosmetics()
+  addNotification({
+    group: 'main',
+    title: 'Developer mode deactivated',
+    text: 'Developer mode has been disabled',
+    type: 'success',
+  })
+}
 
 const listTypes = computed(() => {
   const types = tags.value.projectTypes.map((type) => {
@@ -164,3 +224,88 @@ const listTypes = computed(() => {
   return types
 })
 </script>
+<style scoped lang="scss">
+.theme-options {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: var(--gap-lg);
+}
+.theme-option {
+  width: 100%;
+  border-radius: var(--radius-md);
+  padding: 0;
+  overflow: hidden;
+  border: 1px solid var(--color-divider);
+  background-color: var(--color-button-bg);
+  color: var(--color-base);
+
+  &.selected {
+    color: var(--color-contrast);
+
+    .label svg {
+      color: var(--color-brand);
+    }
+  }
+
+  .preview {
+    background-color: var(--color-bg);
+    padding: 1.5rem;
+
+    .example-card {
+      margin: 0;
+      padding: 1rem;
+      display: grid;
+      grid-template: 'icon text1' 'icon text2';
+      grid-template-columns: auto 1fr;
+      gap: 0.5rem;
+
+      .example-icon {
+        grid-area: icon;
+        width: 2rem;
+        height: 2rem;
+        background-color: var(--color-button-bg);
+        border-radius: var(--radius-sm);
+      }
+
+      .example-text-1,
+      .example-text-2 {
+        height: 0.5rem;
+        border-radius: var(--radius-sm);
+      }
+
+      .example-text-1 {
+        grid-area: text1;
+        width: 100%;
+        background-color: var(--color-base);
+      }
+
+      .example-text-2 {
+        grid-area: text2;
+        width: 60%;
+        background-color: var(--color-secondary);
+      }
+    }
+  }
+
+  .label {
+    display: flex;
+    align-items: center;
+    padding: var(--gap-md) var(--gap-lg);
+
+    svg {
+      margin-right: 0.5rem;
+    }
+  }
+}
+
+.developer-message {
+  svg {
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+
+  .btn {
+    margin-top: var(--gap-sm);
+  }
+}
+</style>
